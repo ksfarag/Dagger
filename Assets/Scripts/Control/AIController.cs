7eba5862d7a3a54628aaee030ef8023df7d0b683
ksfarag <ksfarag@mun.ca>
2022-06-17 22:26:15 -0230

    Adding patrolling behavior to AI and creating visualization for each waypoint

--- Assets/Scripts/Control/AIController.cs
+++ Assets/Scripts/Control/AIController.cs
@@ -1,4 +1,6 @@
 ﻿using RPG.Combat;
+using RPG.Movement;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,26 +10,90 @@ namespace RPG.Control
     public class AIController : MonoBehaviour
     {
         [SerializeField] float chaseDistance = 5f;
+        [SerializeField] float suspicionTime;
+        [SerializeField] PatrolPath patrolPath;
+        [SerializeField] float waypointToleranceDistance = 1f;
+        [SerializeField] float waypointCooldownTime;
+
+
         Fighter fighter;
+        Mover mover;
         GameObject player;
+
+        Vector3 guardPos;
+        float timePlayerWasLastSeen = Mathf.Infinity;
+        float timeSinceArrivedAtWaypoint = 0;
+        int currentWaypointIndex = 0;
 
         private void Start()
         {
             fighter = GetComponent<Fighter>();
             player = GameObject.FindWithTag("Player");
+            guardPos = transform.position;
+            mover = GetComponent<Mover>();
         }
         private void Update()
         {
 
             if (inAttackRange() && fighter.CanAttack(player))
             {
-                fighter.Attack(player);
+                Attack();
             }
-            else 
+            else if (timePlayerWasLastSeen <= suspicionTime)
             {
-                fighter.Cancel();
+                SusBehavior();
             }
+            else
+            {
+                Patrol();
+            }
+            timePlayerWasLastSeen += Time.deltaTime;
+            timeSinceArrivedAtWaypoint += Time.deltaTime;
+        }
 
+        private void Patrol()
+        {
+            Vector3 nextPos = guardPos;
+            if (patrolPath != null)
+            {
+                if (AtWaypoint())
+                {
+                    timeSinceArrivedAtWaypoint = 0;
+                    CycleWaypoint();
+                }
+                nextPos = CurrentWaypoint();
+            }
+            if (timeSinceArrivedAtWaypoint > waypointCooldownTime)
+            {
+                mover.startMoveToAction(nextPos);
+            }
+        }
+
+        private bool AtWaypoint()
+        {
+            float distanceToWaypoint = Vector3.Distance(transform.position, CurrentWaypoint());
+            return distanceToWaypoint < waypointToleranceDistance;
+        }
+
+        private void CycleWaypoint()
+        {
+            currentWaypointIndex = patrolPath.GetNextIndex(currentWaypointIndex);
+        }
+
+        private Vector3 CurrentWaypoint()
+        {
+            return patrolPath.GetWaypoint(currentWaypointIndex);
+        }
+
+        private void SusBehavior()
+        {
+            fighter.Cancel();
+        }
+
+        private void Attack()
+        {
+            timePlayerWasLastSeen = 0;
+            fighter.Attack(player);
         }
 
         //checks if player is in the enemy AI attack range
@@ -36,6 +102,13 @@ namespace RPG.Control
 
             float distanceToPlayer= Vector3.Distance(player.transform.position, transform.position);
             return distanceToPlayer < chaseDistance;
+        }
+
+        //called by Unity
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(transform.position, chaseDistance);
         }
     }
 }
